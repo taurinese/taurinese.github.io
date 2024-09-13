691f6d317f55989975dc679495dd100017993f40
Enzo Taurines <enzodylan.taurines@gmail.com>
2024-09-13 16:29:34 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `taurinese portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
